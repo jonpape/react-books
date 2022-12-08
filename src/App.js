2df250e172d927e04839f2a4d68588bab5fed68c
Jonathan Pape <jonpape@seas.upenn.edu>
2022-12-07 20:26:51 -0800

    Pre server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 function App() {
+    const [books, setBooks] = useState([]);
+
+    const editBookById = (id, newTitle) => {
+        const updateBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, title: newTitle };
+            }
+            return book;
+        });
+        setBooks(updateBooks);
+    };
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+        setBooks(updatedBooks);
+    };
+
+    const createBook = (title) => {
+        const updatedBooks = [...books, {id: Math.round(Math.random()*9999), title }];
+        setBooks(updatedBooks);
+        console.log('Need to ad book with title: ',title);
+    };
+
     return (
-    <div>App</div>
+    <div className="app">
+        <h1>Reading List</h1>
+        <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
+        <BookCreate onCreate={createBook} />
+    </div>
     );
 
 };
---
+++ src/test.js
@@ -0,0 +1,8 @@
+function calculateDaysBetweenDates(begin, end) {
+    var beginDate = new Date(begin);
+    var endDate = new Date(end);
+    var days = Math.round((endDate - beginDate) / (1000 * 60 * 60 * 24));
+    return days;
+
+}
+
